43b92ba83f0b1e2a33ecbeac39fb69c27faa8fb8
Kevin M <kevinmalikf@gmail.com>
2024-03-12 00:45:59 +0700

    mengumpulkan tugas modul 3, asprak kak kukuh

---
+++ modul-3/200441100014-KevinMalikFajar/tourismapp/app/src/main/java/com/vinz/tourismapp/ui/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.vinz.tourismapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vinz.tourismapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ modul-3/200441100014-KevinMalikFajar/tourismapp/app/src/main/java/com/vinz/tourismapp/ui/fragment/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.vinz.tourismapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vinz.tourismapp.R
+import com.vinz.tourismapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
