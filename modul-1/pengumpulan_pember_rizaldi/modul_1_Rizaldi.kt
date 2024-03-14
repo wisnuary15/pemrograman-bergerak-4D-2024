90f7d7c78d3357cab6a9c753cd949e91b62ac190
Rizaldi110 <akhmadrizaldi1429@gmail.com>
2024-03-14 17:24:33 +0700

    tugase Rizal

---
+++ modul-1/pengumpulan_pember_rizaldi/modul_1_Rizaldi.kt
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+    android:orientation="vertical"
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/main"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity"
+    android:padding="25">
+
+
+    <EditText
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:hint=" DENAH RUANGAN"
+        android:textAlignment="center"
+        android:textSize="30"
+        android:textStyle="bold">
+    </EditText>
+
+    <LinearLayout
+        android:layout_width="365dp"
+        android:layout_height="118dp"
+        android:padding="15dp">
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku Dosen">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            >
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            >
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="`">
+        </TextView>
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="365dp"
+        android:layout_height="118dp"
+        android:padding="15dp">
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 1">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 2">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 3">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 4">
+        </TextView>
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="365dp"
+        android:layout_height="118dp"
+        android:padding="15dp">
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 1">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 2">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 3">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 4">
+        </TextView>
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="365dp"
+        android:layout_height="118dp"
+        android:padding="15dp">
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 1">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 2">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 3">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 4">
+        </TextView>
+    </LinearLayout>
+
+    <LinearLayout
+        android:layout_width="365dp"
+        android:layout_height="118dp"
+        android:padding="15dp">
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 1">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 2">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 3">
+        </TextView>
+
+        <TextView
+            android:layout_width="90dp"
+            android:layout_height="86dp"
+            android:text="Bangku 4">
+        </TextView>
+    </LinearLayout>
+</LinearLayout>
