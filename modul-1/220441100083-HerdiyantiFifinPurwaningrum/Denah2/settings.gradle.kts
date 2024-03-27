0fdb7dfc50096de6dc5cc99c47b6809aac7e8488
herdiyantififin <fifinherdiyanti22@gmail.com>
2024-03-27 10:56:40 +0700

    220441100083_Herdiyanti Fifin Purwaningrum-Modul 1-Kak Tamam

---
+++ modul-1/220441100083-HerdiyantiFifinPurwaningrum/Denah2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Denah2"
+include(":app")
+ 
