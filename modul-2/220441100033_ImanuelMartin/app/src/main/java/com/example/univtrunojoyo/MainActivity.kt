cff1cf3d2f50ae3d28a6fa8b5ff2c18ac8bd1e83
martinhara-trunojoyo <martinharahap02@gmail.com>
2024-04-03 13:54:12 +0700

    220441100033_Imanuel Martin_Modul2_kak Angga

---
+++ modul-2/220441100033_ImanuelMartin/app/src/main/java/com/example/univtrunojoyo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.univtrunojoyo
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun lanjut(view: View?) {
+        val intent = Intent(this, activityBeranda::class.java)
+        startActivity(intent)
+    }
+}
