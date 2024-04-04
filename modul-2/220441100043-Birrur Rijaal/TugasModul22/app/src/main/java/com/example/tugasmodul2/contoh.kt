84e644dd7a300a83d952e910226bb93e5470adfd
BirrurRijaal <birrururrijaal@gmail.com>
2024-04-04 15:03:57 +0700

    Modul2_220441100043_BirrurRijaal

---
+++ modul-2/220441100043-Birrur Rijaal/TugasModul22/app/src/main/java/com/example/tugasmodul2/contoh.kt
@@ -0,0 +1,11 @@
+package com.example.tugasmodul2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contoh : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contoh)
+    }
+}
